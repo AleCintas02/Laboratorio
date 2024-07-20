aece635be69ae30e02d7386b2f767b2c40a00a25
Alejandro Cintas <alecintas003@hotmail.com>
2024-07-20 19:18:32 -0300

    botones navbar funcionalidad

--- resources/js/Components/BotonConOpciones.jsx
+++ resources/js/Components/BotonConOpciones.jsx
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
 import "../../css/Boton.css"; // Importa tu archivo CSS
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const BotonConOpciones = ({ label, opciones, isOpen, onMenuClick, url }) => {
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                onMenuClick(); // Llama a la función para cerrar el menú
+            }
+        };
+
+        if (isOpen) {
+            document.addEventListener("click", handleClickOutside);
+        } else {
+            document.removeEventListener("click", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, [isOpen, onMenuClick]);
+
     const handleClick = () => {
         if (url) {
             window.location.href = url;
@@ -18,7 +38,7 @@ const BotonConOpciones = ({ label, opciones, isOpen, onMenuClick, url }) => {
     };
 
     return (
-        <div className="boton-con-opciones">
+        <div className="boton-con-opciones" ref={menuRef}>
             <button className="boton-label" onClick={handleClick}>
                 {label}{" "}
                 {opciones && <i className="bi bi-chevron-down arrow"></i>}
